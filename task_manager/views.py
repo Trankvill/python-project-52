752967d603a671ff6ff17d514204a584c1b18b4a
Konstantin Chupin <chupin.k@yandex.ru>
2023-03-03 17:45:37 +0500

    fix views.py

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,6 +1,33 @@
 from django.views.generic import TemplateView
+from django.contrib import messages
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.utils.translation import gettext as _
 
 
 class IndexView(TemplateView):
 
     template_name = 'index.html'
+
+
+class LoginUserView(SuccessMessageMixin, LoginView):
+
+    template_name = 'form.html'
+    success_message = _('You successfully logged in')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = _('Login')
+        context['button_text'] = _('Login')
+        return context
+
+
+class LogoutUserView(SuccessMessageMixin, LogoutView):
+
+    def dispatch(self, request, *args, **kwargs):
+        messages.add_message(
+            request,
+            messages.INFO,
+            _('You logged out successfully')
+        )
+        return super().dispatch(request, *args, **kwargs)
