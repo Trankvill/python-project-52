a016b8b73b89e3ab8091877a92fc873fcda06af3
Konstantin Chupin <chupin.k@yandex.ru>
2023-03-06 18:17:31 +0500

    fix models.py in statuses

--- task_manager/statuses/models.py
+++ task_manager/statuses/models.py
@@ -10,6 +10,7 @@ class Status(models.Model):
     class Meta:
         verbose_name = _('status')
         verbose_name_plural = _('statuses')
+        ordering = ['id']
 
     def __str__(self):
         return self.name
