3adfa9968e67d8fdfe1ff253954d7ce0ec366814
Константин Чупин <104760286+Trankvill@users.noreply.github.com>
2023-03-06 15:30:40 +0500

    Update views.py

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -57,5 +57,5 @@ class DeleteUserView(UserMixin, DeleteView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = _('Delete a user')
-        context['button_text'] = _('Delete')
+        context['button_text'] = _('Yes, delete')
         return context
