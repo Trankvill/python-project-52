fe8a16d21460ca5e5662b588117c875d9f6d8c3d
Konstantin Chupin <chupin.k@yandex.ru>
2023-03-08 17:06:34 +0500

    fix views

--- task_manager/views.py
+++ task_manager/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.messages.views import SuccessMessageMixin
 from django.utils.translation import gettext as _
+from django.urls import reverse_lazy
 
 
 class IndexView(TemplateView):
@@ -14,7 +15,7 @@ class LoginUserView(SuccessMessageMixin, LoginView):
 
     template_name = 'form.html'
     success_message = _('You successfully logged in')
-
+    next_page = reverse_lazy('index')
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = _('Login')
@@ -23,6 +24,8 @@ class LoginUserView(SuccessMessageMixin, LoginView):
 
 
 class LogoutUserView(SuccessMessageMixin, LogoutView):
+
+    next_page = reverse_lazy('index')
 
     def dispatch(self, request, *args, **kwargs):
         messages.add_message(
