f55c6e72dc0af21daf0746641f5429a6e5864464
Константин Чупин <104760286+Trankvill@users.noreply.github.com>
2023-03-08 14:36:27 +0500

    Update settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -126,7 +126,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'ru'
+LANGUAGE_CODE = os.getenv('LOCALE', default='ru')
 
 TIME_ZONE = 'Europe/Moscow'
 
